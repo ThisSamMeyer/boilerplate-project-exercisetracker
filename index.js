132a7b833714d4ce678c6a8b01503d0c0423efc7
Samantha Meyer <slmeyer90@gmail.com>
2022-11-26 11:33:23 -0500

    configure GET users request to only show user info

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 
 // Mount body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false } ));
@@ -27,21 +27,18 @@ let User = mongoose.model("User", userSchema);
 
 // Render root path file
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// Handle POST requests to create new user:
+// Handle POST requests to /api/users:
 //  initialize New user model
 //  save to db
 //  return username and _id
 app.post('/api/users', (req, res) => {
-
-  let username = req.body.username
-
+  let username = req.body.username;
   let user = new User({
     username: username
-  })
-
+  });
   user.save((saveErr, userSaved) => {
     if (saveErr) {
       console.log('save() error');
@@ -51,22 +48,30 @@ app.post('/api/users', (req, res) => {
       "username": userSaved.username,
       "_id": userSaved._id
     });
-
   }); // user.save() block
-}); // new user post block
+}); // POST new user block
+
 
 // Handle GET requests to /api/users
 //  display list of all users
 //  format will be an array of object literals
 app.get('/api/users', (req,res) => {
+  let userList = [];
   User.find((findErr, usersFound) => {
     if (findErr) {
       console.log('User.find() error');
       console.error(findErr);
     }
-    res.json(usersFound);
-  })
-})
+    usersFound.forEach(user => {
+      userList.push({
+        _id: user._id,
+        username: user.username,
+        __v: user.__v
+      })
+    })
+    res.json(userList);
+  }); // User.find() block
+}); // GET all users block
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
