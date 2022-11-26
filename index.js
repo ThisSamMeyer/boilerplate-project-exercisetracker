7b6a86098a5644ee28e7cf7b991469f528b8d34a
Samantha Meyer <slmeyer90@gmail.com>
2022-11-26 10:37:02 -0500

    handle new user post requests

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.static('public'))
+
+// Mount body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false } ));
+
+// Connect to database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define mongoose schema for user records
+const Schema = mongoose.Schema;
+const userSchema = new Schema ({
+  username: {
+    type: String, required: true
+  }
+});
+
+// Define mongoose model for user records
+let User = mongoose.model("User", userSchema);
+
+// Render root path file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Handle create new user post requests:
+//  initialize New user model
+//  save to db
+//  return username and _id
+app.post('/api/users', (req, res) => {
 
+  let username = req.body.username
 
+  let user = new User({
+    username: username
+  })
+
+  user.save((saveErr, userSaved) => {
+    if (saveErr) {
+      console.log('save() error');
+      console.error(saveErr);
+    }
+    res.json({
+      "username": userSaved.username,
+      "_id": userSaved._id
+    });
+    
+  }); // user.save() block
+}); // new user post block
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
